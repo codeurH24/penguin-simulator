b3aee18f94c3e82089ef560197680b8f8094634d
Florent Corlouër <cci.corlouer@gmail.com>
2025-06-06 13:21:46 +0200

    ✅ Ajouter tests complets pour les variables bash
    - Créer suite de tests pour la gestion des variables (15 tests)
      * Test détection assignation (var=value)
      * Test création variables simples et multiples
      * Test substitution  et
      * Test variables d'environnement (/home/codeurh24, /home/codeurh24/project/learningh24/app/public/assets/templates/modules/bash, codeurh24)
      * Test cas complexes et caractères spéciaux
    
    - Corriger getEnvironmentVariables() pour nouveau système contexte
      * Utiliser getCurrentPath() au lieu de currentPath
      * Assurer compatibilité avec ancien système
      * Fixer variables /home/codeurh24/project/learningh24/app/public/assets/templates/modules/bash et /home/codeurh24/project/learningh24/app/public/assets/templates/modules
    
    - Intégrer tests dans pipeline principal
      * Ajouter stage bash/variables.stages.js
      * Mettre à jour stages.js principal
    
    Tous les tests passent (15/15) ✅

--- test-cases/stages.js
+++ test-cases/stages.js
@@ -5,8 +5,10 @@ import { stages as commandRmStages } from './stages/commands/rm.stages.js';
 import { stages as commandMvStages } from './stages/commands/mv.stages.js';
 import { stages as commandCdStages } from './stages/commands/cd.stages.js';
 import { stages as commandPwdStages } from './stages/commands/pwd.stages.js';
+import { stages as bashVariablesStages } from './stages/bash/variables.stages.js';
 
 export function stages(suites) {
+    bashVariablesStages(suites);
     commandMkdirStages(suites);
     commandLsStages(suites);
     commandTouchStages(suites);
